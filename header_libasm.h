ae48d07c07d8ea1d654cc196872b3f45766ecb75
gbouwen <gijsbouwen@gmail.com>
2020-07-10 15:13:11 +0200

    removed unneccessary functions

--- header_libasm.h
+++ header_libasm.h
@@ -6,7 +6,7 @@
 /*   By: gbouwen <marvin@codam.nl>                    +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/07/01 14:56:19 by gbouwen       #+#    #+#                 */
-/*   Updated: 2020/07/03 15:38:23 by gbouwen       ########   odam.nl         */
+/*   Updated: 2020/07/10 15:12:57 by gbouwen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,5 @@ char	*ft_strcpy(char *dst, const char *src);
 ssize_t	ft_write(int fildes, const void *buf, size_t nbyte);
 ssize_t	ft_read(int fildes, void *buf, size_t nbyte);
 char	*ft_strdup(const char *s1);
-
-int		ft_atoi_base(char *str, char *base);
-//void	ft_list_push_front(t_list **begin_list, void *data);
-//int	ft_list_size(t_list *begin_list);
-//void	ft_list_sort(t_list **begin_list, int (*cmp)());
-//void 	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)());
 
 #endif
